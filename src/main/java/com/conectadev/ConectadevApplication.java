93f406eaa2157b15891a3e527593eed7d59d9a62
Fernando Carta UBUNTU <cartafernando@outlook.com>
2024-03-27 14:41:48 -0500

    Back Java ConectaDev red Social POLI

---
+++ src/main/java/com/conectadev/ConectadevApplication.java
@@ -0,0 +1,13 @@
+package com.conectadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectadevApplication.class, args);
+	}
+
+}
